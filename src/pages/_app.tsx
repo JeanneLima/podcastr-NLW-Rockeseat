e9ce53db462927a70bdc0ac3721068b7138e06bb
JeanneLima <jeannelima.profissional@gmail.com>
2021-04-23 11:09:34 -0700

    Reorganiza conteúdo do provider do Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,22 @@
 import '../styles/global.scss'
 
 import Header from '../components/Header'
+import Player from '../components/Player'
 
 import styles from '../styles/app.module.scss'
-import Player from '../components/Player'
-import { PlayerContext } from '../contexts/playerContext'
-import { useState } from 'react'
+import { PlayerContextProvider } from '../contexts/playerContext'
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
-    <div className={styles.wrapper}>
-      <main>
-        <Header/>
-        <Component {...pageProps} />
-      </main>
-      <Player/>
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header/>
+          <Component {...pageProps} />
+        </main>
+        <Player/>
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   )
 }
 
